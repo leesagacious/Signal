46268a14f10f46f6fa048e3b6d684952a3b3cca5
leesagacious <sensor1010@163.com>
2018-01-18 21:51:53 +0800

    Update sigds.c

--- sigds.c
+++ sigds.c
@@ -1,8 +1,15 @@
+#include <linux/types.h>
 
+/*
 #define _OUTSTANDING 64
 #define _NORMALSIG   64
 #define _OUTSTANDING_WORDS (_OUTSTANDING / _NORMALSIG)
 
+1 ： The corresponding signal in this position is in the pending state
+0 ： The corresponding signal in this position is not in the pending state
+                                                                    -leesagacious
 typedef _struct {
   unsigned long bitsig[_OUTSTANDING_WORDS];
 }outstanding_mask;
+*/
+DECLARE_BITMAP(bitsig, 2)
